47c64fc1a37f2ed4ee763722f8516fed3b11ed7b
ASdeveloper-app <arturo.araujo.alvarez@gmail.com>
2020-04-10 16:44:31 -0500

    Estructura para implementar REDUX

--- src/index.js
+++ src/index.js
@@ -1,18 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {BrowserRouter as Router} from "react-router-dom";
-
-import './index.css';
-
-import App from './App';
-
 import * as serviceWorker from './serviceWorker';
+// Estilos
+import './index.css';
+// Rutas
+import App from './App';
+// Redux
+import { Provider } from 'react-redux';
+import store from './redux/store'
 
 ReactDOM.render(
   <React.StrictMode>
-    <Router>
-      <App />
-    </Router>
+    <Provider store={store}>
+      <Router>
+        <App />
+      </Router>
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
---
+++ src/redux/store.js
@@ -0,0 +1,29 @@
+import { createStore, applyMiddleware, combineReducers } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import thunk from 'redux-thunk'
+
+// Muestra de productos
+import { showWatches } from './reducers/watches'
+import { showShirts } from './reducers/shirts'
+import { showVideogames } from './reducers/videogames.js'
+
+// Buscador de productos
+import watchFinder from './reducers/watchFinder';
+import shirtFinder from './reducers/shirtFinder';
+import videogameFinder from './reducers/videogameFinder';
+
+const reducers = combineReducers({
+    watch: showWatches,
+    shirt: showShirts,
+    videogames: showVideogames,
+    watchFinder,
+    shirtFinder,
+    videogameFinder
+});
+
+const store = createStore(
+    reducers,
+    composeWithDevTools(applyMiddleware(thunk))
+);
+
+export default store;
