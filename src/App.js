12a41050b9dffac4ad1f2dcf49eb0cc64dbfa02c
ASdeveloper-app <arturo.araujo.alvarez@gmail.com>
2020-04-10 13:20:44 -0500

    Pruebas de Inicio exitosas

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './ArturoAraujo';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
+        <img src='./ArturoAraujo.jpeg' className="App-logo" alt="logo" />
+        <h1>
+          Bienvenido Candidato 01
+        </h1>
+        <h2
           className="App-link"
-          href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
-        </a>
+          Version 1.0
+        </h2>
       </header>
     </div>
   );
